b5e94d58dd56c717f20608d5c738f984733ad736
Joel Strait <joelstrait@hotmail.com>
2009-07-17 22:26:17 -0400

    Refactored code to use the WaveFile gem for writing wave files. Additional refactoring to simplify and shorten the code.

--- clawhammer.rb
+++ clawhammer.rb
@@ -22,30 +22,18 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+require 'rubygems'
+require 'wavefile'
+
 SAMPLES_PER_HUB = 6
 
-# Wave header constants.
 # Hammerhead assumes that all samples are:
-#   16-bit
 #   1 channel (mono)
 #   44100 samples per second (sample rate)
-# Therefore, all output Wave files will have an identical header,
-# expect for the payload size.
-#
-# For more on Wave file specification, see:
-#   http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
-CHUNK_ID = "RIFF"
-FORMAT = "WAVE"
-SUB_CHUNK1_ID = "fmt "
-SUB_CHUNK1_SIZE = 16
-AUDIO_FORMAT = 1
-SUB_CHUNK2_ID = "data"
-HEADER_SIZE = 36
-NUM_CHANNELS = 1
-BITS_PER_SAMPLE = 16
-BYTE_RATE = 88200
-BLOCK_ALIGN = 2
+#   16-bit
+NUM_CHANNELS = :mono
 SAMPLE_RATE = 44100
+BITS_PER_SAMPLE = 16
 
 def main
   if ARGV[0] == nil
@@ -55,77 +43,25 @@ def main
     puts ""
     exit()
   else
-    raw_file_data = read_hub_file(ARGV[0])
-  end
-  
-	samples = []
-	hub_title = ""
-	SAMPLES_PER_HUB.times{ |i|	  
-    # HUB Format:
-    #             0: Length of HUB title
-    #          1-30: HUB title. If HUB title is less than 30 bytes, remaining bytes are garbage.
-    #         31-34: Length (n) sample data for next sample, unsigned little endian format.
-    #            35: Flag for whether sample should be looped when played in Hammerhead. Ignored by Clawhammer.
-    #   36-(n + 36): Raw sample data
-    # This format is repeated six times, once for each sample
-  
-		header = raw_file_data.slice!(0..30)
+    hub_file = File.open(ARGV[0], "rb")
     
-    # Since each sample header is identical,
-    # only parse the first one.
-		if i == 0
-			hub_title_length = header[0]
-			hub_title = header[1..hub_title_length]
-			puts "HUB Title: #{hub_title}"
-		end
-	
-		sample_length = raw_file_data.slice!(0..3).unpack("V1")[0]
-		
-		# Ignore the loop flag byte
-		raw_file_data.slice!(0)
-		
-		samples[i] = raw_file_data.slice!(0...sample_length)
-		save_wave_file("samples/#{hub_title}-#{i + 1}.wav", samples[i])
-		puts "Sample #{i + 1} extracted, #{sample_length} bytes."
-	}
-end
-
-def save_wave_file(file_name, sample_data)
-  # For info on Wave file specification, see:
-  #   http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
-  
-  data_length = sample_data.length
-  
-  # Wave Header
-  file_contents = CHUNK_ID
-  file_contents += [HEADER_SIZE + data_length].pack("V")
-  file_contents += FORMAT
-  file_contents += SUB_CHUNK1_ID
-  file_contents += [SUB_CHUNK1_SIZE].pack("V")
-  file_contents += [AUDIO_FORMAT].pack("v")
-  file_contents += [NUM_CHANNELS].pack("v")
-  file_contents += [SAMPLE_RATE].pack("V")
-  file_contents += [BYTE_RATE].pack("V")
-  file_contents += [BLOCK_ALIGN].pack("v")
-  file_contents += [BITS_PER_SAMPLE].pack("v")
-  file_contents += SUB_CHUNK2_ID
-  file_contents += [data_length].pack("V")
-  # Wave Payload
-  file_contents += sample_data
-  
-  wave_file = File.open(file_name, "w")
-  wave_file.syswrite(file_contents)
-  wave_file.close
-end
-
-def read_hub_file(file_name)
-	raw_file_data = ""
-  
-	hub = File.open(file_name)
-	hub.each {|line| raw_file_data += line}
-	hub.close()
-	
-	return raw_file_data
+    SAMPLES_PER_HUB.times{ |i|
+      hub_title_length = hub_file.sysread(1).unpack("c1")[0]
+      hub_title = hub_file.sysread(30).slice(0...hub_title_length)
+      
+      num_samples = hub_file.sysread(4).unpack("V1")[0]
+      
+      # Ignore the stretch flag
+      hub_file.sysread(1)
+      
+      w = WaveFile.new(NUM_CHANNELS, SAMPLE_RATE, BITS_PER_SAMPLE)
+      w.sample_data = hub_file.sysread(num_samples).unpack("s*")
+      w.save("#{hub_title}-#{i + 1}.wav")
+      puts "Sample #{i + 1} extracted, #{num_samples} bytes."
+    }
+    
+    hub_file.close()
+  end
 end
 
 main()
