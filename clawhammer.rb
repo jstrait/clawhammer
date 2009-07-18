b1c82ba1e3453a47b88191875db3df96a7a81213
Joel Strait <joelstrait@hotmail.com>
2009-07-18 18:07:07 -0400

    Removed vestigial main() function. Clarified usage of sample vs. sound in comments and variable names.

--- clawhammer.rb
+++ clawhammer.rb
@@ -22,12 +22,12 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-require 'rubygems'
+require 'rubygems'  # For Ruby pre-1.9
 require 'wavefile'
 
-SAMPLES_PER_HUB = 6
+SOUNDS_PER_HUB = 6
 
-# Hammerhead assumes that all samples are:
+# Hammerhead assumes that all sounds are:
 #   1 channel (mono)
 #   44100 samples per second (sample rate)
 #   16-bit
@@ -35,43 +35,39 @@ NUM_CHANNELS = :mono
 SAMPLE_RATE = 44100
 BITS_PER_SAMPLE = 16
 
-def main
-  if ARGV[0] == nil
-    puts ""
-	  puts "Usage:"
-    puts "  ruby clawhammer.rb [path of *.hub file]"
-    puts ""
-    exit()
-  else
-    hub_file = File.open(ARGV[0], "rb")
+if ARGV[0] == nil
+  puts ""
+  puts "Usage:"
+  puts "  ruby clawhammer.rb [path of *.hub file]"
+  puts ""
+  exit()
+else
+  hub_file = File.open(ARGV[0], "rb")
+  
+  SOUNDS_PER_HUB.times{ |i|
+    # HUB Header Format:
+    #   0: Length of HUB title
+    #   1-30: HUB title. If HUB title is less than 30 bytes, remaining bytes are garbage.
+    #   31-34: Size of sample data in bytes, unsigned little endian format.
+    #   35: Flag for whether sound should be stretched to fill a measure when played in Hammerhead.
+    #       Ignored by Clawhammer.
     
-    SAMPLES_PER_HUB.times{ |i|
-      # HUB Header Format:
-      #   0: Length of HUB title
-      #   1-30: HUB title. If HUB title is less than 30 bytes, remaining bytes are garbage.
-      #   31-34: Size of sample data in bytes, unsigned little endian format.
-      #   35: Flag for whether sample should be stretched to fill a measure when played in Hammerhead.
-      #       Ignored by Clawhammer.
-      
-      # Read HUB title
-      hub_title_length = hub_file.sysread(1).unpack("c1")[0]
-      hub_title = hub_file.sysread(30).slice(0...hub_title_length)
-      
-      # Read sample data size
-      sample_data_length = hub_file.sysread(4).unpack("V1")[0]
-      
-      # Ignore the stretch flag
-      hub_file.sysread(1)
-      
-      # Read sample data and write wave file
-      w = WaveFile.new(NUM_CHANNELS, SAMPLE_RATE, BITS_PER_SAMPLE)
-      w.sample_data = hub_file.sysread(sample_data_length).unpack("s*")
-      w.save("#{hub_title}-#{i + 1}.wav")
-      puts "Sample #{i + 1} extracted, #{sample_data_length} bytes."
-    }
+    # Read HUB title
+    hub_title_length = hub_file.sysread(1).unpack("c1")[0]
+    hub_title = hub_file.sysread(30).slice(0...hub_title_length)
     
-    hub_file.close()
-  end
+    # Read sample data size
+    sample_data_length = hub_file.sysread(4).unpack("V1")[0]
+    
+    # Ignore the stretch flag
+    hub_file.sysread(1)
+    
+    # Read sample data and write wave file
+    w = WaveFile.new(NUM_CHANNELS, SAMPLE_RATE, BITS_PER_SAMPLE)
+    w.sample_data = hub_file.sysread(sample_data_length).unpack("s*")
+    w.save("#{hub_title}-#{i + 1}.wav")
+    puts "Sound #{i + 1} extracted, #{sample_data_length} bytes."
+  }
+  
+  hub_file.close()
 end
-
-main()
